7c1b52372160be17b8f03b7f2935c74e05cf848f
VamShop <vamshop@gmail.com>
2019-10-20 21:57:24 +0300

    Корректировка языка по умолчанию

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-process.env.LANGUAGE = 'ru';
+process.env.LANGUAGE = 'en';
 
 // config used by server side only
 const dbHost = process.env.DB_HOST || '127.0.0.1';
